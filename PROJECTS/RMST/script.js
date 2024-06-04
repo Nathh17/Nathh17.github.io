9a46fa29df23543a7171c17d10144996aa389d40
Nathh17 <ultrazocker21@gmail.com>
2024-06-04 15:58:00 +0200

    update point system

--- PROJECTS/RMST/script.js
+++ PROJECTS/RMST/script.js
@@ -1,5 +1,23 @@
 const buttons = document.querySelectorAll('.btn');
 const outputDiv = document.querySelector('.output');
+const decreaseBtn = document.querySelector('.decrease');
+const increaseBtn = document.querySelector('.increase');
+const increaseValue = document.querySelector('.increaseValue');
+
+let currentName = "";
+let counterValue = 0;
+
+decreaseBtn.addEventListener('click', () => {
+  counterValue--;
+  increaseValue.textContent = counterValue;
+});
+
+increaseBtn.addEventListener('click', () => {
+  counterValue++;
+  increaseValue.textContent = counterValue;
+});
+
+let deletingText = false; // Flag per controllare se deleteText è in esecuzione
 
 // Load the JSON file
 fetch('data/data.json')
@@ -15,9 +33,14 @@ fetch('data/data.json')
       button.addEventListener('click', () => {
         const nameLabel = button.querySelector('.nameLabel');
         const labelText = nameLabel.textContent;
+        currentName = nameLabel.textContent;
         const points = getPoints(labelText);
-        var output = labelText + ": " + points;
-        deleteText(outputDiv, output);
+        const output = labelText + ": " + points;
+        // Se deleteText non è in esecuzione, chiamala
+        if (!deletingText) {
+          deletingText = true;
+          deleteText(outputDiv, output);
+        }
       });
     });
   });
@@ -25,14 +48,34 @@ fetch('data/data.json')
 function deleteText(element, text, i = element.textContent.length - 1) {
   if (i >= 0) {
     element.textContent = element.textContent.substring(0, i);
-    setTimeout(() => deleteText(element, text, i - 1), 125);
+    setTimeout(() => deleteText(element, text, i - 1), 75);
   } else {
     typingEffect(element, text, 0);
+    deletingText = false; // Imposta la flag a false dopo che deleteText è completata
   }
 }
 
 function typingEffect(element, text, i = 0) {
   element.textContent += text[i];
   if (i === text.length - 1) return;
-  setTimeout(() => typingEffect(element, text, i + 1), 125);
+  setTimeout(() => typingEffect(element, text, i + 1), 75);
+}
+
+function updateRegister() {
+  // Apriamo il file JSON
+  fetch('data/data.json')
+    .then(response => response.json())
+    .then(data => {
+      // Aggiorniamo il valore del counter per il nome corrente
+      if (currentName in data) {
+        data[currentName] += counterValue;
+      }
+      // Convertiamo l'oggetto JSON in una stringa
+      const jsonString = JSON.stringify(data);
+      // Creiamo un oggetto XMLHttpRequest per inviare la richiesta di update
+      const xhr = new XMLHttpRequest();
+      xhr.open('PUT', 'data/data.json', true);
+      xhr.setRequestHeader('Content-Type', 'application/json');
+      xhr.send(jsonString);
+    });
 }
